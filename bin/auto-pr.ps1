a4654221a602692d9b13de48cac15ef2bddbc6b5
Muhammad Fadhlika <git@fadhlika.com>
2024-06-03 11:17:02 +0700

    change upstream

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "mfadhlika/scoop-bucket:main"
+    [String]$upstream = "mfadhlika/scoop-bucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
